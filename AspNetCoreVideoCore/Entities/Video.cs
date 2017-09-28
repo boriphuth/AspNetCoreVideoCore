84cd1d8d22fdff233b9c611bf977731689ed28b6
Jonas <N/A>
2017-09-28 04:33:58 +0200

    Refactoring the Application

--- AspNetCoreVideoCore/Entities/Video.cs
+++ AspNetCoreVideoCore/Entities/Video.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreVideoCore.Models;
 
 namespace AspNetCoreVideoCore.Entities
 {
@@ -9,6 +10,6 @@ namespace AspNetCoreVideoCore.Entities
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public int GenreId { get; set; }
+        public Genres Genre { get; set; }
     }
 }
