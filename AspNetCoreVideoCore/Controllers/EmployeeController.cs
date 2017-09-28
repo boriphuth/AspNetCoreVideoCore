17d62493fb2495b74b00f9580f2a4b2bbf371839
Jonas <N/A>
2017-09-28 03:50:47 +0200

    Route attribute on each action method

--- AspNetCoreVideoCore/Controllers/EmployeeController.cs
+++ AspNetCoreVideoCore/Controllers/EmployeeController.cs
@@ -1,20 +1,26 @@
-﻿namespace AspNetCoreVideoCore.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreVideoCore.Controllers
 {
+    [Route("employee")]
     public class EmployeeController
     {
+        [Route("")]
+        public string Index()
+        {
+            return "Hello from Employee";
+        }
+        [Route("name")]
         public string Name()
         {
             return "Jonas";
         }
 
+        [Route("country")]
         public string Country()
         {
             return "Sweden";
         }
 
-        public string Index()
-        {
-            return "Hello from Employee";
-        }
     }
 }
