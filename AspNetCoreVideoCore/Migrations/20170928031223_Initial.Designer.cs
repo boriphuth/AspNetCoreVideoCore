15f418f47a545a4b85a5ae55d5c51f21cee6bbdf
Jonas <N/A>
2017-09-28 05:14:22 +0200

    Adding the Initial Migration and Creating the Database

---
+++ AspNetCoreVideoCore/Migrations/20170928031223_Initial.Designer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using AspNetCoreVideoCore.Data;
+using AspNetCoreVideoCore.Models;
+
+namespace AspNetCoreVideoCore.Migrations
+{
+    [DbContext(typeof(VideoDbContext))]
+    [Migration("20170928031223_Initial")]
+    partial class Initial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("AspNetCoreVideoCore.Entities.Video", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("Genre");
+
+                    b.Property<string>("Title")
+                        .IsRequired()
+                        .HasMaxLength(80);
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Videos");
+                });
+        }
+    }
+}
